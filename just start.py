ceb59a3a98fa90c7dc19d956f6bc876e38813687
Sudarshen <sudarshenk@gmail.com>
2025-06-23 11:13:48 +0200

    Secondo impacto

--- just start.py
+++ just start.py
@@ -1,31 +1,31 @@
-# Bismillah 
+# # Bismillah 
 
-print(f"Hello world")
+# print(f"Hello world")
 
-#Bismillah
+# #Bismillah
 
-print(f"Hellow WWooooorld!!!")
+# print(f"Hellow WWooooorld!!!")
 
-# print is a function 
-# String is a sequence of characters. you always see a string by quote's. and use double quotes
-print('I Am Iron Man.')
+# # print is a function 
+# # String is a sequence of characters. you always see a string by quote's. and use double quotes
+# print('I Am Iron Man.')
 
-print("No, I am Tony Stark!")
-print("No, i am Poppy")
-#We are practicing python... its awesome.
+# print("No, I am Tony Stark!")
+# print("No, i am Poppy")
+# #We are practicing python... its awesome.
 
 
-# Mulitple line strings
-print(f"""I am Iron Man" 
-No, i am Tony Stark 
-No, I am Poppy.""")
-# do triple quotations marks 
+# # Mulitple line strings
+# print(f"""I am Iron Man" 
+# No, i am Tony Stark 
+# No, I am Poppy.""")
+# # do triple quotations marks 
 
-print(f"I am Iron Man.\
- No, I am Tony Stark.\
+print(f"I am Iron Man.
+ No, I am Tony Stark.
  No, I am Poppy.")
 
-# Concatenating and new line indicator charachter.
-print(f"I am Iron Man. \n" + "No, I am Tony Stark. \n" + "No, I am Poppy.")
+# # Concatenating and new line indicator charachter.
+# print(f"I am Iron Man. \n" + "No, I am Tony Stark. \n" + "No, I am Poppy.")
 
-
+# print(f"I am Poppy. \n" * 100)
