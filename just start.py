d44c85f99f9f668f456a12f1923fb37a34098b41
Sudarshen <sudarshenk@gmail.com>
2025-06-30 17:20:55 +0200

    Did today Episode 2, and was a pretty succesfull day.

--- just start.py
+++ just start.py
@@ -21,9 +21,9 @@
 # No, I am Poppy.""")
 # # do triple quotations marks 
 
-print(f"I am Iron Man.
- No, I am Tony Stark.
- No, I am Poppy.")
+# print(f"I am Iron Man.
+#  No, I am Tony Stark.
+#  No, I am Poppy.")
 
 # # Concatenating and new line indicator charachter.
 # print(f"I am Iron Man. \n" + "No, I am Tony Stark. \n" + "No, I am Poppy.")
@@ -34,3 +34,39 @@ print(f"I am Iron Man.
 
 # yooohooohoooooo , yookozoo.
 
+# episode 2 to become dangerous in python.
+
+# Let's build a robot barista
+
+print(f"Hello, Welcome to PythonCafee!!!")
+
+client_name = input(f"what is your name?\n")
+
+print(f"Hello {client_name}, thank you so much for coming in today!!")
+
+# # name = "abdullah"
+# # print(name)
+
+# # name = "IronMan"
+# # print(name)
+
+
+# client_order = input(f"""{client_name} please choose what you would like to order from this menu:
+# Espresso        $4
+# Cafe Latte      $6
+# Americano       $6.5
+# Cappuccino      $5
+# Macchiato       $8\n""")
+
+
+menu = """
+Espresso        $4
+Cafe Latte      $6
+Americano       $6.5
+Cappuccino      $5
+Macchiato       $8\n"""
+
+client_order = input(f"{client_name} please choose what you would like to order from this menu: {menu}")
+
+
+print(f"{client_order} is a excellent choice {client_name}, please take a seat while we are making your order.")
